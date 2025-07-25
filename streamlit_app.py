3756c03a3bb92b93a3124b9c133b2385ed042361
Nijesu-alt <testimonyobanijesumi@gmail.com>
2025-07-25 23:53:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,15 @@ with st.expander('Data'):
   df = pd.read_csv(url)
   df
 
+with st.sidebar:
+  st.sidebar.markdown("<h3 style='color:#F63366;'>Customer Profile</h3>", unsafe_allow_html=True)
+  st.header('Input Features')
+  no_of_dep = st.slider('Number of Dependents', 0, 10, 5)
+  income = st.number_input('Annual Income', 100000, 10000000, 100000)
+  loan_amount = st.number_input('Loan Amount', 300000, 50000000, 1000000, 100000)
+  score = st.slider('Cibil Score', 2, 20, 12)
+  res_ass_val = st.number_input('Residential Asset Value', 100000, 40000000, 20000000, 500000)
+  com_ass_val = st.number_input('Commercial Asset Value', 0, 20000000, 500000, 500000)
+  lux_ass_val = st.number_input('Luxury Asset Value', 200000, 50000000, 1000000, 500000)
+  bank_ass_val = st.number_input('Bank Asset Value', 0, 20000000, 500000, 100000)
+  
