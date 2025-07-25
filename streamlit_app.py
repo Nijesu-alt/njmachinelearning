76a7f14fa783557979dbaa35668ffea4e91ec435
Nijesu-alt <testimonyobanijesumi@gmail.com>
2025-07-25 23:28:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,9 @@ st.title('🪙 Loan Eligibility App')
 
 st.info('This apps uses machine learning model to check loan eligibility')
 
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  url = "https://raw.githubusercontent.com/Nijesu-alt/njmachinelearning/refs/heads/master/loan_approval_dataset.csv"
+  df = pd.read_csv(url)
+  df
 
