2ce3dfa32b02c11b24e4a6f0459753f7e3f6026e
Nijesu-alt <testimonyobanijesumi@gmail.com>
2025-07-27 13:12:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.sidebar:
   income = st.number_input('Annual Income', 100000, 10000000, 100000, 500000)
   loan_amount = st.number_input('Loan Amount', 300000, 50000000, 1000000, 100000)
   loan_term = st.slider('Loan Term', 2, 40, 25)
-  score = st.slider('Cibil Score', 2, 20, 12)
+  score = st.slider('Cibil Score', 200, 1000, 500)
   res_ass_val = st.number_input('Residential Asset Value', 100000, 40000000, 20000000, 500000)
   com_ass_val = st.number_input('Commercial Asset Value', 0, 20000000, 500000, 500000)
   lux_ass_val = st.number_input('Luxury Asset Value', 200000, 50000000, 1000000, 500000)
